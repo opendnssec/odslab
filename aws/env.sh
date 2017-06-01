bab3a6ea45b5c87a5e481600647f760f31ceced5
Jakob Schlyter <jakob@kirei.se>
2017-06-01 14:19:25 +0200

    bump

--- aws/env.sh
+++ aws/env.sh
@@ -13,7 +13,7 @@ export ODS_KEYPAIR_STUDENT="ods-student-20170601"
 # Maximum is currently 90 groups (180 instances)
 # More can be requested here:
 # http://aws.amazon.com/contact-us/ec2-request/
-export ODS_STUDENT_COUNT=15
+export ODS_STUDENT_COUNT=10
 
 # Network settings
 export ODS_EC2_SG_ID=sg-14a9fd72
@@ -21,6 +21,6 @@ export ODS_EC2_VPC_ID=vpc-87a187e3
 export ODS_EC2_SUBNET_ID=subnet-eaf785b2
 
 # update with latest 64-bit EBS AMI from https://cloud-images.ubuntu.com/locator/ec2/
-# (search for "eu-west amd64 ebs")
-export ODS_EC2_AMI=ami-ef141f89
+# (search for "eu-west amd64 ebs hvm")
+export ODS_EC2_AMI=ami-6c101b0a
 export ODS_EC2_TYPE=t2.micro
